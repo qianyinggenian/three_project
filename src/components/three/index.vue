543ded0a82fe629d5aa531c4a53d1809fe0a1268
wangkangzhang <1368504885@qq.com>
2022-09-01 11:50:22 +0800

    更新

--- src/components/three/index.vue
+++ src/components/three/index.vue
@@ -50,6 +50,8 @@ export default {
       renderer.render(scene, camera);
       // 创建轨道控制器
       const controls = new OrbitControls(camera, renderer.domElement);
+      // 设置控制器的阻尼,让控制器更加逼真，必须在循环调用是调用update();
+      controls.enableDamping = true;
       console.log(controls);
       // 添加坐标轴
       const axesHelper = new THREE.AxesHelper(30);
@@ -88,12 +90,24 @@ export default {
       });
 
       function render () {
+        controls.update();
         renderer.render(scene, camera);
         // 默认传入时间
         requestAnimationFrame(render);
       }
 
       render();
+      // 监听画面变化，更新玄滩画面
+      window.addEventListener('resize', () => {
+        // 更新摄像头
+        camera.aspect = window.innerWidth / window.innerHeight;
+        // 更新摄像机投影矩阵
+        camera.updateProjectionMatrix();
+        // 更新渲染器
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        // 设置渲染器的像素比
+        renderer.setPixelRatio(window.devicePixelRatio);
+      });
     }
   }
 };
