9613b770a810909779bd0fe1891345cb95da4fb5
wangkangzhang <1368504885@qq.com>
2022-09-01 15:59:26 +0800

    gui的使用

--- src/components/three/index.vue
+++ src/components/three/index.vue
@@ -6,6 +6,8 @@
 import * as THREE from 'three';
 // 导入动画库
 import gsap from 'gsap';
+// 导入dat.gui
+import * as dat from 'dat.gui';
 // 导入轨道控制器
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
@@ -40,6 +42,40 @@ export default {
       // 旋转
       cube.rotation.set(Math.PI / 4, 0, 0);
       scene.add(cube);
+      const gui = new dat.GUI();
+      gui.add(cube.position, 'x').min(0).max(5).step(0.01).name('移动X轴').onChange((value) => {
+        console.log('值被修改：', value);
+      }).onFinishChange((value) => {
+        console.log('完全停下来:', value);
+      });
+      // 修改颜色
+      const params = {
+        color: '#ffff00',
+        fn: () => {
+          // 让立方体运动起来
+          gsap.to(cube.position, {
+            x: 5,
+            duration: 5,
+            // 设置重复次数, 无数次循环 -1
+            repeat: -1,
+            // 设置往返运动
+            yoyo: true
+          });
+        }
+      };
+      gui.addColor(params, 'color').onChange((value) => {
+        console.log('值被修改：', value);
+        cube.material.color.set(value);
+      }).onFinishChange((value) => {
+        console.log('完全停下来:', value);
+      });
+      // 设置选项框
+      gui.add(cube, 'visible').name('是否显示');
+      var folder = gui.addFolder('设置立方体');
+      folder.add(cube.material, 'wireframe');
+      // 设置按钮点击触发某个事件
+      folder.add(params, 'fn').name('立方体运动');
+      console.log(folder);
       // 添加渲染器
       const renderer = new THREE.WebGLRenderer();
       // 设置渲染器大小
@@ -55,29 +91,6 @@ export default {
       // 添加坐标轴
       const axesHelper = new THREE.AxesHelper(30);
       scene.add(axesHelper);
-      // 设置动画
-      gsap.to(cube.position, {
-        x: 5,
-        duration: 5,
-        // 设置重复次数, 无数次循环 -1
-        repeat: -1,
-        // 设置往返运动
-        yoyo: true,
-        // delay,设置延迟2秒运动
-        delay: 2,
-        ease: 'power1.inOut',
-        onComplete: () => {
-          console.log('动画完成');
-        },
-        onStart: () => {
-          console.log('动画开始');
-        }
-      });
-      gsap.to(cube.rotation, {
-        x: 2 * Math.PI,
-        duration: 5,
-        ease: 'power1.inOut'
-      });
       window.addEventListener('dblclick', () => {
         // if (animate1.isActive()) {
         //   // 暂停
@@ -115,6 +128,31 @@ export default {
         renderer.setSize(window.innerWidth, window.innerHeight);
         // 设置渲染器的像素比
         renderer.setPixelRatio(window.devicePixelRatio);
+      });
+    },
+    gsapFn (cube) {
+      // 设置动画
+      gsap.to(cube.position, {
+        x: 5,
+        duration: 5,
+        // 设置重复次数, 无数次循环 -1
+        repeat: -1,
+        // 设置往返运动
+        yoyo: true,
+        // delay,设置延迟2秒运动
+        delay: 2,
+        ease: 'power1.inOut',
+        onComplete: () => {
+          console.log('动画完成');
+        },
+        onStart: () => {
+          console.log('动画开始');
+        }
+      });
+      gsap.to(cube.rotation, {
+        x: 2 * Math.PI,
+        duration: 5,
+        ease: 'power1.inOut'
       });
     }
   }
