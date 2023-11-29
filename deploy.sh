63b8cc74ec3a7e86bd9d308cd940b0c9ad8a59d7
wangkangzhang <1368504885@qq.com>
2023-11-29 10:50:20 +0800

    更新

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ git commit -m 'deploy'
 # 部署到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
- 部署到 https://qianyinggenian.github.io
+# 部署到 https://qianyinggenian.github.io
  git push -f git@github.com:qianyinggenian/qianyinggenian.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
