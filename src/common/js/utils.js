57f0e2c69518fd056ee0f4203218fb008b57b591
wangkangzhang <1368504885@qq.com>
2023-11-29 18:01:33 +0800

    更新

--- src/common/js/utils.js
+++ src/common/js/utils.js
@@ -1,4 +1,4 @@
-export function getResourcePath (path,systemCode='three_project') {
+export function getResourcePath (path, systemCode= 'three_project') {
   let urlPath = '';
   if (process.env.NODE_ENV !== 'development') {
     urlPath = `/${systemCode}${path}`;
