5fe898f68322ec5c22e0059de36aa1033f40f491
wangkangzhang <1368504885@qq.com>
2023-11-29 10:31:16 +0800

    更新

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
   publicPath: './',
   // publicPath: process.env.NODE_ENV === 'production'
   //   ? '/yian/' : './',
-  outputDir: 'docs',
+  outputDir: '/docs',
   devServer: {
     port: 8888,
     proxy: {
